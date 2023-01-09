964919dfd072d367e19a9f2c6f78efb5c27b1176
alan <alanfm61226@gmail.com>
2023-01-09 18:14:08 +0800

    fix assign_key bug

--- src/thrust_sort_optimize.cu
+++ src/thrust_sort_optimize.cu
@@ -44,11 +44,14 @@ __global__ void test_get_int(const uint64_t *d_source, node_type *d_test,
 __global__ void assign_key(const uint64_t *d_source,
                            const size_type *d_start_pos, node_type *d_keys,
                            size_type *d_seq_id, const size_type position,
+                           const size_type first_seq_id,
                            const size_type seqs_size, const int t_width) {
   size_type idx = blockIdx.x * blockDim.x + threadIdx.x;
   if (idx < seqs_size) {
     d_keys[idx] = get_int(
-        d_source, (d_start_pos[d_seq_id[idx]] + position) * t_width, t_width);
+        d_source,
+        (d_start_pos[d_seq_id[idx] - first_seq_id] + position) * t_width,
+        t_width);
   }
   return;
 }
@@ -67,20 +70,6 @@ radix_sort(const text_type &source, std::vector<size_type> &sequence_id,
   std::chrono::steady_clock::time_point begin, end;
   begin = std::chrono::steady_clock::now();
 
-  // lambda function to get the nearest number of power of 2
-  /*
-  auto round_up_32 = [](unsigned int v) {
-    --v;
-    v |= v >> 1;
-    v |= v >> 2;
-    v |= v >> 4;
-    v |= v >> 8;
-    v |= v >> 16;
-    ++v;
-    return v;
-  };
-  */
-
   std::vector<std::vector<std::pair<size_type, node_type>>> sorted_seqs(
       total_nodes);
 
@@ -89,6 +78,7 @@ radix_sort(const text_type &source, std::vector<size_type> &sequence_id,
   const uint8_t t_width = source.width();
   const size_type source_size_byte = source.bit_size() / 8 + 1;
   const size_type seqs_size = (*start_pos_map).size(); // total sequence size
+  size_type first_seq_id = sequence_id[0];
 
   // copy source to device memory
   // cudaMallocHost((void **)&source, source_size_byte, cudaHostAllocDefault);
@@ -133,6 +123,7 @@ radix_sort(const text_type &source, std::vector<size_type> &sequence_id,
   end = std::chrono::steady_clock::now();
   init_time = std::chrono::duration_cast<std::chrono::microseconds>(end - begin)
                   .count();
+
   for (size_type position = 0; seqs_left > 0; ++position) {
     //---- Assign Keys ----//
     begin = std::chrono::steady_clock::now();
@@ -140,7 +131,8 @@ radix_sort(const text_type &source, std::vector<size_type> &sequence_id,
         (seqs_left + thread_per_block - 1) / thread_per_block;
     assign_key<<<block_per_grid, thread_per_block>>>(
         d_source, d_start_pos, d_keys_raw + arr_start_idx,
-        d_seq_id_raw + arr_start_idx, position, seqs_left, t_width);
+        d_seq_id_raw + arr_start_idx, position, first_seq_id, seqs_left,
+        t_width);
     end = std::chrono::steady_clock::now();
     key_time +=
         std::chrono::duration_cast<std::chrono::microseconds>(end - begin)
@@ -193,98 +185,6 @@ radix_sort(const text_type &source, std::vector<size_type> &sequence_id,
       node_type next_node_id = source[(*start_pos_map)[seq_id] + position + 1];
       sorted_seqs[h_keys_vec[i] - 1].push_back({seq_id, next_node_id});
     }
-    // const int unroll_num = 8;
-    /*
-    for (size_type i = end_counter; i < (seqs_left / unroll_num);
-         i += unroll_num) {
-      size_type seq_id = h_seq_id[i];
-      node_type next_node_id = source[(*start_pos_map)[seq_id] + position + 1];
-      sorted_seqs[h_keys_vec[i] - 1].push_back({seq_id, next_node_id});
-
-      size_type seq_id1 = h_seq_id[i + 1];
-      node_type next_node_id1 =
-          source[(*start_pos_map)[seq_id1] + position + 1];
-      sorted_seqs[h_keys_vec[i + 1] - 1].push_back({seq_id1, next_node_id1});
-
-      size_type seq_id2 = h_seq_id[i + 2];
-      node_type next_node_id2 =
-          source[(*start_pos_map)[seq_id2] + position + 1];
-      sorted_seqs[h_keys_vec[i + 2] - 1].push_back({seq_id2, next_node_id2});
-
-      size_type seq_id3 = h_seq_id[i + 3];
-      node_type next_node_id3 =
-          source[(*start_pos_map)[seq_id3] + position + 1];
-      sorted_seqs[h_keys_vec[i + 3] - 1].push_back({seq_id3, next_node_id3});
-
-      size_type seq_id4 = h_seq_id[i + 4];
-      node_type next_node_id4 =
-          source[(*start_pos_map)[seq_id4] + position + 1];
-      sorted_seqs[h_keys_vec[i + 4] - 1].push_back({seq_id4, next_node_id4});
-
-      size_type seq_id5 = h_seq_id[i + 5];
-      node_type next_node_id5 =
-          source[(*start_pos_map)[seq_id5] + position + 1];
-      sorted_seqs[h_keys_vec[i + 5] - 1].push_back({seq_id5, next_node_id5});
-
-      size_type seq_id6 = h_seq_id[i + 6];
-      node_type next_node_id6 =
-          source[(*start_pos_map)[seq_id6] + position + 1];
-      sorted_seqs[h_keys_vec[i + 6] - 1].push_back({seq_id6, next_node_id6});
-
-      size_type seq_id7 = h_seq_id[i + 7];
-      node_type next_node_id7 =
-          source[(*start_pos_map)[seq_id7] + position + 1];
-      sorted_seqs[h_keys_vec[i + 7] - 1].push_back({seq_id7, next_node_id7});
-
-      size_type seq_id1 = h_seq_id[i + 1];
-      node_type next_node_id1 = source[(*start_pos_map)[seq_id1] + position +
-      1];
-      sorted_seqs[h_keys_vec[i + 1] - 1].push_back({seq_id1, next_node_id1});
-
-      size_type seq_id1 = h_seq_id[i + 1];
-      node_type next_node_id1 = source[(*start_pos_map)[seq_id1] + position +
-      1];
-      sorted_seqs[h_keys_vec[i + 1] - 1].push_back({seq_id1, next_node_id1});
-
-      size_type seq_id1 = h_seq_id[i + 1];
-      node_type next_node_id1 = source[(*start_pos_map)[seq_id1] + position +
-      1];
-      sorted_seqs[h_keys_vec[i + 1] - 1].push_back({seq_id1, next_node_id1});
-
-      size_type seq_id1 = h_seq_id[i + 1];
-      node_type next_node_id1 = source[(*start_pos_map)[seq_id1] + position +
-      1];
-      sorted_seqs[h_keys_vec[i + 1] - 1].push_back({seq_id1, next_node_id1});
-
-      size_type seq_id1 = h_seq_id[i + 1];
-      node_type next_node_id1 = source[(*start_pos_map)[seq_id1] + position +
-      1];
-      sorted_seqs[h_keys_vec[i + 1] - 1].push_back({seq_id1, next_node_id1});
-
-      size_type seq_id1 = h_seq_id[i + 1];
-      node_type next_node_id1 = source[(*start_pos_map)[seq_id1] + position +
-      1];
-      sorted_seqs[h_keys_vec[i + 1] - 1].push_back({seq_id1, next_node_id1});
-
-      size_type seq_id1 = h_seq_id[i + 1];
-      node_type next_node_id1 = source[(*start_pos_map)[seq_id1] + position +
-      1];
-      sorted_seqs[h_keys_vec[i + 1] - 1].push_back({seq_id1, next_node_id1});
-
-      size_type seq_id1 = h_seq_id[i + 1];
-      node_type next_node_id1 = source[(*start_pos_map)[seq_id1] + position +
-      1];
-      sorted_seqs[h_keys_vec[i + 1] - 1].push_back({seq_id1, next_node_id1});
-  }
-  */
-    /*
-    size_type start = (seqs_left / unroll_num) * unroll_num;
-    for (size_type i = end_counter + start; i < seqs_left + end_counter; ++i) {
-      size_type seq_id = h_seq_id[i];
-      node_type next_node_id = source[(*start_pos_map)[seq_id] + position + 1];
-      sorted_seqs[h_keys_vec[i] - 1].push_back({seq_id, next_node_id});
-    }
-    */
     end = std::chrono::steady_clock::now();
     place_time +=
         std::chrono::duration_cast<std::chrono::microseconds>(end - begin)
