4f1ce2d3d4b8a15a913b90a0fc64b1308a477b28
Jouni Siren <jouni.siren@iki.fi>
2026-08-14 19:20:53 -0700

    Deprecated stuff in tests

--- tests/test_variants.cpp
+++ tests/test_variants.cpp
@@ -1,27 +1,3 @@
-/*
-  Copyright (c) 2018 Jouni Siren
-
-  Author: Jouni Siren <jouni.siren@iki.fi>
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
-*/
-
 #include <set>
 
 #include <gtest/gtest.h>
@@ -218,7 +194,7 @@ VariantTestData basic_data
   }
 };
 
-INSTANTIATE_TEST_CASE_P(BasicHaplotypes, VariantTest, ::testing::Values(basic_data));
+INSTANTIATE_TEST_SUITE_P(BasicHaplotypes, VariantTest, ::testing::Values(basic_data));
 
 //------------------------------------------------------------------------------
 
@@ -251,7 +227,7 @@ VariantTestData overlap_data
   }
 };
 
-INSTANTIATE_TEST_CASE_P(OverlappingVariants, VariantTest, ::testing::Values(overlap_data));
+INSTANTIATE_TEST_SUITE_P(OverlappingVariants, VariantTest, ::testing::Values(overlap_data));
 
 //------------------------------------------------------------------------------
 
@@ -282,7 +258,7 @@ VariantTestData overlap_skip_data
   }
 };
 
-INSTANTIATE_TEST_CASE_P(SkipOverlaps, VariantTest, ::testing::Values(overlap_skip_data));
+INSTANTIATE_TEST_SUITE_P(SkipOverlaps, VariantTest, ::testing::Values(overlap_skip_data));
 
 //------------------------------------------------------------------------------
 
